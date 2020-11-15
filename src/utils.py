d28f901d64901fee760e21816bd28cd717a02048
Benjamin Lowe <ben.lowe.uk@googlemail.com>
2020-11-15 20:26:27 +0900

    Revert "feat: add requirements.txt and fine-tuning instructions"

--- src/utils.py
+++ src/utils.py
@@ -1,32 +1,13 @@
 # Third Party
 import librosa
 import numpy as np
-import time as timelib
-import scipy
-import soundfile as sf
-import scipy.signal as sps
-from scipy import interpolate
+
 # ===============================================
 #       code from Arsha for loading data.
 # ===============================================
 def load_wav(vid_path, sr, mode='train'):
-
-    #t1=timelib.time()
-    #print("start loading wav")
-    #print(sr)
-    #wav, sr_ret = librosa.load(vid_path, sr=sr)
-    #sr_ret, wav = scipy.io.wavfile.read(vid_path)
-
-    wav, sr_ret = sf.read(vid_path)
-    #sr_ret, old_audio = scipy.io.wavfile.read(vid_path)
-    #if sr_ret != sr:
-    #    new_rate = sr
-    #    number_of_samples = round(len(old_audio) * float(new_rate) / sr_ret)
-    #    wav = sps.resample(old_audio, number_of_samples)
-
-
-    #assert sr_ret == 16000, "we need same samplerate as librosa originally provided but is: " +str(sr_ret)
-    #print("finish loading wav", timelib.time()-t1)
+    wav, sr_ret = librosa.load(vid_path, sr=sr)
+    assert sr_ret == sr
     if mode == 'train':
         extended_wav = np.append(wav, wav)
         if np.random.random() < 0.3:
@@ -43,8 +24,6 @@ def lin_spectogram_from_wav(wav, hop_length, win_length, n_fft=1024):
 
 
 def load_data(path, win_length=400, sr=16000, hop_length=160, n_fft=512, spec_len=250, mode='train'):
-    #print("starting loading a datum")
-    #t1 = timelib.time()
     wav = load_wav(path, sr=sr, mode=mode)
     linear_spect = lin_spectogram_from_wav(wav, hop_length, win_length, n_fft)
     mag, _ = librosa.magphase(linear_spect)  # magnitude
@@ -61,7 +40,6 @@ def load_data(path, win_length=400, sr=16000, hop_length=160, n_fft=512, spec_le
     # preprocessing, subtract mean, divided by time-wise var
     mu = np.mean(spec_mag, 0, keepdims=True)
     std = np.std(spec_mag, 0, keepdims=True)
-    #print("finished loading a datum", timelib.time() - t1)
     return (spec_mag - mu) / (std + 1e-5)
 
 
